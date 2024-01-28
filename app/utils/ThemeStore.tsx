84cda9c7ff0808f59e52e591ef147f043ea0dc86
esmakocak <kerem.4555@gmail.com>
2024-01-28 14:54:12 +0300

    bug fix

--- app/utils/ThemeStore.tsx
+++ app/utils/ThemeStore.tsx
@@ -1,36 +1,32 @@
 import { create } from "zustand";
-import { persist, StateStorage } from "zustand/middleware";
+import { createJSONStorage, persist, StateStorage } from "zustand/middleware";
 
 interface ThemeState {
   theme: "light" | "dark";
   setTheme: (theme: "light" | "dark") => void;
 }
+
 export const useThemeStore = create(
   persist<ThemeState>(
     (set) => {
-      const prefersDarkMode =
-        typeof window !== "undefined" &&
-        localStorage.getItem("theme") === "dark"
-          ? true
-          : window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-      const defaultTheme: "light" | "dark" = prefersDarkMode ? "dark" : "light";
+      // const prefersDarkMode = window.matchMedia(
+      //   "(prefers-color-scheme: dark)"
+      // ).matches;
+      // const prefersDarkMode = false;
+      // const defaultTheme: "light" | "dark" = prefersDarkMode ? "dark" : "light";
+      // get the default Theme from sessionstorage
+      const defaultTheme: "light" | "dark" =
+        (sessionStorage.getItem("theme-storage") as "light" | "dark") ||
+        "light";
 
       return {
         theme: defaultTheme,
-        setTheme: (theme) => {
-          set({ theme });
-          // Save the theme to localStorage only if running on the client side
-          if (typeof window !== "undefined") {
-            localStorage.setItem("theme", theme);
-          }
-        },
+        setTheme: (theme) => set({ theme }),
       };
     },
     {
       name: "theme-storage",
-      // Specify the type of storage
-      getStorage: () => localStorage as StateStorage,
+      storage: createJSONStorage(() => sessionStorage), // Specify the type of storage
     }
   )
 );
