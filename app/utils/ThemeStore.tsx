95f501012c6079914b5ab401be888f2d9ad55978
esmakocak <kerem.4555@gmail.com>
2024-01-28 14:36:09 +0300

    buf fix

--- app/utils/ThemeStore.tsx
+++ app/utils/ThemeStore.tsx
@@ -5,23 +5,32 @@ interface ThemeState {
   theme: "light" | "dark";
   setTheme: (theme: "light" | "dark") => void;
 }
-
 export const useThemeStore = create(
   persist<ThemeState>(
     (set) => {
-      const prefersDarkMode = window.matchMedia(
-        "(prefers-color-scheme: dark)"
-      ).matches;
+      const prefersDarkMode =
+        typeof window !== "undefined" &&
+        localStorage.getItem("theme") === "dark"
+          ? true
+          : window.matchMedia("(prefers-color-scheme: dark)").matches;
+
       const defaultTheme: "light" | "dark" = prefersDarkMode ? "dark" : "light";
 
       return {
         theme: defaultTheme,
-        setTheme: (theme) => set({ theme }),
+        setTheme: (theme) => {
+          set({ theme });
+          // Save the theme to localStorage only if running on the client side
+          if (typeof window !== "undefined") {
+            localStorage.setItem("theme", theme);
+          }
+        },
       };
     },
     {
       name: "theme-storage",
-      getStorage: () => localStorage as StateStorage, // Specify the type of storage
+      // Specify the type of storage
+      getStorage: () => localStorage as StateStorage,
     }
   )
 );
